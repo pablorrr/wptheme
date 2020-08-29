fe878b6ffc7933c48c837957432ab8cfd509f251
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2020-08-29 09:50:13 +0200

    modif plugin comapatibiltity

--- inc/plugin-compatibility/plugin-compatibility.php
+++ inc/plugin-compatibility/plugin-compatibility.php
@@ -4,8 +4,3 @@
  */
 
 require get_template_directory() . '/inc/plugin-compatibility/jetpack/jetpack.php';
-
-/**
- * Load Woocommerce compatibility file.
- */
-require get_template_directory() . '/inc/plugin-compatibility/woocommerce/woocommerce.php';
